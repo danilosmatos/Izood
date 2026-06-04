37827b2ef8ddef5ad68187c810127339b01270aa
Vinicius Augusto <168447584+Vinicius1213@users.noreply.github.com>
2026-06-04 16:43:09 -0300

    Create Contador de Passos.py

---
+++ game/Contador de Passos.py
@@ -0,0 +1,17 @@
+class ContadorDePassosJogo:
+    def __init__(self):
+        self.passos_efetuados = 0
+
+    def registrar_passo(self):
+        """Incrementa o contador a cada movimento validado."""
+        self.passos_efetuados += 1
+
+    def zerar_contador(self):
+        self.passos_efetuados = 0
+
+  # UI simplesdo contador  
+  def desenhar_contador(self, tela, x=20, y=20):
+        """Desenha o contador de passos principal do gameplay na tela."""
+        fonte = pygame.font.SysFont(None, 30)
+        texto = fonte.render(f"Passos Dados: {self.passos_efetuados}", True, (255, 255, 0)) # Amarelo
+        tela.blit(texto, (x, y))
