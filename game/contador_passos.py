cd694e2cfedae2dfb517252ed3368587a77a982c
Danilo <danilosoaresmatos@gmail.com>
2026-06-04 17:49:23 -0300

    ajustando importações, concertando indentação e renomeando arquivos

--- game/contador_passos.py
+++ game/contador_passos.py
@@ -1,3 +1,5 @@
+import pygame
+
 class ContadorDePassosJogo:
     def __init__(self):
         self.passos_efetuados = 0
@@ -10,7 +12,7 @@ class ContadorDePassosJogo:
         self.passos_efetuados = 0
 
   # UI simplesdo contador  
-  def desenhar_contador(self, tela, x=20, y=20):
+    def desenhar_contador(self, tela, x=20, y=20):
         """Desenha o contador de passos principal do gameplay na tela."""
         fonte = pygame.font.SysFont(None, 30)
         texto = fonte.render(f"Passos Dados: {self.passos_efetuados}", True, (255, 255, 0)) # Amarelo
--- game/entrega.py
+++ game/entrega.py
@@ -1,3 +1,5 @@
+import pygame
+
 class SistemaEntrega:
     def __init__(self):
         pass
